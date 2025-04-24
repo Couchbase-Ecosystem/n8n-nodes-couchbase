dc0e99aebb0aae140d4169ecb9aa553232e30144
Elliot Scribner <elliot.scribner@couchbase.com>
2025-04-24 16:26:31 -0700

    Bump to v1.1.0

--- nodes/Couchbase/Couchbase.node.ts
+++ nodes/Couchbase/Couchbase.node.ts
@@ -69,7 +69,7 @@ export class Couchbase implements INodeType {
 		name: 'couchbase',
 		icon: { light: 'file:../icons/couchbase.svg', dark: 'file:../icons/couchbase.dark.svg' },
 		group: ['input'],
-		version: 1.0,
+		version: [1.0, 1.1],
 		subtitle: '={{$parameter["resource"] + ": " + $parameter["operation"]}}',
 		description:
 			'Couchbase node to insert, update, retrieve, and delete data from a Couchbase database using KV, Query and Search services',
